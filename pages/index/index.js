644a1e0c1fcc2cdb79a1e5a2e840725462c532f8
黄靖萱 <huangjingxuan@huangjingxuandeMacBook-Pro.local>
2018-06-05 19:41:52 +0800

    11
    111

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,7 +15,11 @@ Page({
     onlineTime:null,
     offlineTime:null,
     multiparts:null,
-    teach:null,
+    teach:[],
+    statusToString:'',
+    teacherName:null,
+    teacherInfo:null,
+    list:[],
 
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -74,7 +78,7 @@ Page({
     duration: 1000
 
 
-  },
+  }, 
   goToSearch:function(e){
     wx.navigateTo({
       url: '/pages/search/search',
@@ -87,9 +91,64 @@ Page({
     })
   },
   onLoad: function () {
+    var that = this;
+    wx.request({
+      url: 'https://www.sunlikeme.xyz/lesson/list',
+      data: {
+        'unionId': app.globalData.userId,
+        'pageSize': 4,
+        'showOnlyMine': 0,
+        'lessonName':"语文"
+      },
+      header: {
+        "content-type": "application/x-www-form-urlencoded",
+        'unionId': app.globalData.userId
+      },
+      method: "GET",
+      success: function (result) {
+        for (var i = 0; i < result.data.data.length; i++) {
+          var list = that.data.list;
+          var teach = that.data.teach;
+          list.push({
+            header: 'https://www.sunlikeme.xyz' + result.data.data[i]["header"],
+            lessonId: result.data.data[i]["lessonId"],
+            lessonName: result.data.data[i]["lessonName"],
+            introduction: result.data.data[i]["introduction"],
+            status: result.data.data[i]["status"],
+            onlineTime: result.data.data[i]["onlineTime"],
+            offlineTime: result.data.data[i]["offlineTime"],
+            multiparts: result.data.data[i]["multiparts"],
+            teacherInfo: result.data.data[i]["teach"][0]["portrait"],
+            teacherName: result.data.data[i]["teach"][0]["nickName"],
+          });
+          that.setData({
+            list: list,
+          })
+          if (result.data.data[i]["status"] == 0) {
+            that.setData({
+              statusToString: '未开始'
+            })
+          }
+          else if (result.data.data[i]["status"] == 1) {
+            that.setData({
+              statusToString: '直播中'
+            })
+          }
+          else if (result.data.data[i]["status"] == 2) {
+            that.setData({
+              statusToString: '已结束'
+            })
+          }
+        }
+        console.log(that.data.list);
+      },
+      fail: function () {
+        console.log('登陆失败，检查网络连接')
+      }
+    }),
     this.setData({
             userInfo: app.globalData.userInfo,
             hasUserInfo: true
           })
-    }
+    }  
 })
